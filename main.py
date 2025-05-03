7e2256146dabe194c13427cac91b9140312505d7
akt8811 <akt8811@gmail.com>
2025-05-03 11:39:38 +0900

    Update main.py

--- main.py
+++ main.py
@@ -35,5 +35,5 @@ def search_order():
     return response
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 3000))
+    port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port)
